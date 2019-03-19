0bb2a4818b379b195754d2e818f4a0f489ec740d
Xylot <Joseph.meriam@gmail.com>
2019-03-19 15:51:10 -0500

    Added camera view button and progress bar

--- KivyTest.py
+++ KivyTest.py
@@ -4,53 +4,65 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.graphics import *
 from kivy.core.window import Window
+from kivy.uix.progressbar import ProgressBar
 
-class myLayout(FloatLayout):
+
+class Home(FloatLayout):
 	def __init__(self):
-		super(myLayout, self).__init__()
+		super(Home, self).__init__()
 		# Window.clearcolor = (1, 1, 1, 1)
 		self.size = Window.size
 		self.deviceArray = Widget(size=(100,100))
 		self.add_widget(self.deviceArray)
 
-		self.deviceArrayList = []
+		self.setNodeProperties()
 
-		self.activeNode = 2
+		self.recordingProgressBar = ProgressBar(max=1000, Widget.y=10)
+		self.add_widget(self.recordingProgressBar)
+		self.currentProgress = 0
+		self.recordingProgressBar.value = self.currentProgress
 
-		self.populateNodes(2, 7)
-
-		# with self.deviceArray.canvas:
-		# 	for x in range(0,7):
-		# 		if x == self.activeNode:
-		# 			Color(1,0,0,1)
-		# 		else:
-		# 			Color(1,1,1,1)
-		# 		self.deviceArrayList.append(self.createNode(x * 100 + x * 10 + 18, Window.size[1] - 110))
-		# 		self.deviceArrayList.append(self.createNode(x * 100 + x * 10 + 18, Window.size[1] - 220))
+		self.populateNodes(self.nodeSize)
 		
-		self.cameraViewButton = Button(text='My first button', pos=(300, 300), size_hint = (.1,.1))
+		self.cameraViewButton = Button(text='Camera View', pos=(567, Window.size[1] - 475), size_hint = (.268,.4))
+		self.cameraViewButton.bind(on_press=self.changeActiveNode)
 		self.add_widget(self.cameraViewButton)
 
 	def createNode(self, xpos, ypos):
 		return Rectangle(pos=(xpos, ypos))
 
-	def populateNodes(self, rows, cols):
+	def populateNodes(self, size):
 		with self.deviceArray.canvas:
-			for rowNum in range(0,rows):
-				for colNum in range(0, cols):
-					currentNode = rowNum * cols + colNum
+			for rowNum in range(0,size[0]):
+				for colNum in range(0, size[1]):
+					currentNode = rowNum * size[1] + colNum
+					self.recordingProgressBar.value = (currentNode / self.nodeCount) * 1000
 					if currentNode == self.activeNode:
 						Color(1,0,0,1)
 					else:
 						Color(1,1,1,1)
 					self.deviceArrayList.append(self.createNode(colNum * 100 + colNum * 10 + 18, Window.size[1] - (rowNum + 1) * 110))
+	
+	def changeActiveNode(self, instance):
+		self.activeNode = self.activeNode + 1
+		if self.activeNode >= self.nodeCount:
+			self.activeNode = 0
+		self.populateNodes(self.nodeSize)
+
+	def setNodeProperties(self):
+		self.nodeSize = (2, 7)
+		self.activeNode = 0
+		self.nodeCount = self.getNodeCount()
+		self.deviceArrayList = []
+
+	def getNodeCount(self):
+		return self.nodeSize[0] * self.nodeSize[1]
 
 
-
-class MyApp(App):
+class StatusInterface(App):
     def build(self):
-        return myLayout()
+        return Home()
 
 
 if __name__ == '__main__':
-     MyApp().run()
+     StatusInterface().run()
