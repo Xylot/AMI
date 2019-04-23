b76bd840f32eb8d69d6ca0a85df38eddf1d7d72b
Xylot <Joseph.Meriam@gmail.com>
2019-04-23 12:23:35 -0500

    Added intial setup screen

--- KivyTest.py
+++ KivyTest.py
@@ -20,7 +20,7 @@ class Home(FloatLayout):
 	def __init__(self):
 		super(Home, self).__init__()
 		# Window.clearcolor = (1, 1, 1, 1)
-		Window.size = (780, 475)
+		Window.size = (800, 480)
 		self.size = Window.size
 		self.deviceArray = Widget(size=(100,100))
 		self.add_widget(self.deviceArray)
@@ -32,17 +32,22 @@ class Home(FloatLayout):
 
 		self.addCameraViewButton()
 		self.addNodeIncrementButton()
+		self.addSettingsButton()
+		self.addStopButton()
 
 		self.checkbox = CheckBox(pos=(400, Window.size[1] - 300), size=(100,100), size_hint = (.1,.1))
 		#self.checkbox.bind(active=self.showWebcamFeed)
 		#self.add_widget(self.checkbox)
 
 		self.addStatusLabel()
+		self.addTimeLeftLabel()
+		self.addCameraPositionLabel()
 
 		#self.recordNodes()
 
 	def createNode(self, xpos, ypos):
-		return Rectangle(pos=(xpos, ypos))
+		return Line(rounded_rectangle=(xpos, ypos, 100, 100, 10))
+		#return Rectangle(pos=(xpos, ypos))
 
 	def populateNodes(self, size):
 		with self.deviceArray.canvas:
@@ -88,25 +93,49 @@ class Home(FloatLayout):
 		return self.nodeSize[0] * self.nodeSize[1]
 
 	def addProgressbar(self):
-		self.recordingProgressBar = ProgressBar(max=1000, pos=(0, -200), width=100, height=400)
+		self.recordingProgressBar = ProgressBar(max=1000, pos=(0, -220), width=100, height=400)
 		self.add_widget(self.recordingProgressBar)
 		self.currentProgress = 0
 		self.recordingProgressBar.value = self.currentProgress
 
 	def addCameraViewButton(self):
-		self.cameraViewButton = Button(text='Camera View', pos=(567, Window.size[1] - 375), size_hint = (.268,.25))
+		self.cameraViewButton = Button(text='Camera View', pos=(670, Window.size[1] - 330), size_hint = (.125,.20833))
 		self.cameraViewButton.bind(on_press=self.showWebcamFeed)
 		self.add_widget(self.cameraViewButton)
 
 	def addNodeIncrementButton(self):
-		self.nodeIncrement = Button(text='Increment', pos=(10, Window.size[1] - 375), size_hint = (.268,.25))
+		self.nodeIncrement = Button(text='Increment', pos=(670, Window.size[1] - 440), size_hint = (.125,.20833))
 		self.nodeIncrement.bind(on_press=self.changeActiveNode)
 		self.add_widget(self.nodeIncrement)
 
+	def addSettingsButton(self):
+		self.settingsButton = Button(text='Settings', pos=(560, Window.size[1] - 330), size_hint = (.125,.20833))
+		self.settingsButton.bind(on_press=self.changeActiveNode)
+		self.add_widget(self.settingsButton)
+
+	def addStopButton(self):
+		self.stopButton = Button(text='STOP', pos=(560, Window.size[1] - 440), size_hint = (.125,.20833))
+		self.stopButton.background_color = (255,0,0,0.6)
+		self.stopButton.bind(on_press=self.changeActiveNode)
+		self.add_widget(self.stopButton)
+
 	def addStatusLabel(self):
 		self.statusLabel = Label(text='Current status:')
-		self.statusLabel.font_size = '30sp'
+		self.statusLabel.font_size = '25sp'
+		self.statusLabel.pos = (-310, -20)
 		self.add_widget(self.statusLabel)
+
+	def addTimeLeftLabel(self):
+		self.timeLeftLabel = Label(text='Time left in recording:')
+		self.timeLeftLabel.font_size = '25sp'
+		self.timeLeftLabel.pos = (-271, -60)
+		self.add_widget(self.timeLeftLabel)
+
+	def addCameraPositionLabel(self):
+		self.cameraPositionLabel = Label(text='Camera position (X,Y):')
+		self.cameraPositionLabel.font_size = '25sp'
+		self.cameraPositionLabel.pos = (-267, -100)
+		self.add_widget(self.cameraPositionLabel)
 
 	def recordNodes(self):
 		for num, node in enumerate(self.deviceArrayList, start=1):
